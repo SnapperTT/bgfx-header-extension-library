082a802348b104146bb5aa63975a3ab91bc05eb7
Liam Twigger <4939563+SnapperTT@users.noreply.github.com>
2021-08-12 16:20:21 +1000

    switched arg[1], arg[2]... in lua build scripts to table.unpack

--- shaders/atmosphere/build_atmosphere.lua
+++ shaders/atmosphere/build_atmosphere.lua
@@ -1,3 +1,3 @@
 local cs = loadfile("compile_shaders.lua") or loadfile("../compile_shaders.lua");
-cs("src/atmosphere", arg[1], arg[2], arg[3]);
-cs("src/atmosphere_frag", arg[1], arg[2], arg[3]);
+cs("src/atmosphere", table.unpack(arg));
+cs("src/atmosphere_frag", table.unpack(arg));
--- shaders/bloom/build_bloom.lua
+++ shaders/bloom/build_bloom.lua
@@ -1,7 +1,7 @@
 local cs = loadfile("compile_shaders.lua") or loadfile("../compile_shaders.lua");
-cs("src/bloom_brightpass", "-o", "bloom_brightpass", "-vdef", "bloom", arg[1], arg[2], arg[3]);
-cs("src/bloom_brightpass", "-o", "bloom_brightpass_lum", "-vdef", "bloom", "-D", "USE_LUM", arg[1], arg[2], arg[3]);
-cs("src/bloom_blur", "-o", "bloom_blur_hz", "-vdef", "bloom", "-D", "BLUR_HZ", arg[1], arg[2], arg[3]);
-cs("src/bloom_blur", "-o", "bloom_blur_vt", "-vdef", "bloom", "-D", "BLUR_VT", arg[1], arg[2], arg[3]);
+cs("src/bloom_brightpass", "-o", "bloom_brightpass", "-vdef", "bloom", table.unpack(arg));
+cs("src/bloom_brightpass", "-o", "bloom_brightpass_lum", "-vdef", "bloom", "-D", "USE_LUM", table.unpack(arg));
+cs("src/bloom_blur", "-o", "bloom_blur_hz", "-vdef", "bloom", "-D", "BLUR_HZ", table.unpack(arg));
+cs("src/bloom_blur", "-o", "bloom_blur_vt", "-vdef", "bloom", "-D", "BLUR_VT", table.unpack(arg));
 
 
